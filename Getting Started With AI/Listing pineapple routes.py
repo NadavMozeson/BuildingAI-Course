77d4cdefe353c04e0c90f59f7b5f448f8ab50076
NadavMozeson <nadavson02@gmail.com>
2023-01-07 16:51:51 +0200

    finished task - "Listing pineapple routes.py"

--- Getting Started With AI/Listing pineapple routes.py
+++ Getting Started With AI/Listing pineapple routes.py
@@ -3,10 +3,15 @@ portnames = ["PAN", "AMS", "CAS", "NYC", "HEL"]
 
 def permutations(route, ports):
     # Write your recursive code here
+    if len(ports) > 0:
+        remainPorts = len(ports)
+        for i in range(remainPorts):
+            permutations(route+[ports[i]], ports[i+1:]+ports[:i])
 
     # Print the port names in route when the recursion terminates
-    print(' '.join([portnames[i] for i in route]))
+    else:
+        print(' '.join([portnames[i] for i in route]))
 
 
-# This will start the recursion with 0 ("PAN") as the first stop
+# This will start the recursion with 0 "(PAN)" as the first stop
 permutations([0], list(range(1, len(portnames))))
